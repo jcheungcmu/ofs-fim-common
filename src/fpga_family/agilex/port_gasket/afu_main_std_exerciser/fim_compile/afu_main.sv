96b56b378baef8407583ebfed6641c7da596561a
mdeckar1 <102693772+mdeckar1@users.noreply.github.com>
2024-12-10 10:07:23 -0800

    Fix vf1+ port_rst_n attach (#1571)

--- src/fpga_family/agilex/port_gasket/afu_main_std_exerciser/fim_compile/afu_main.sv
+++ src/fpga_family/agilex/port_gasket/afu_main_std_exerciser/fim_compile/afu_main.sv
@@ -347,7 +347,7 @@ end
 generate
    for (genvar link = 0; link < PG_NUM_LINKS; link = link + 1) begin: rst_link
       for (genvar p = 0; p < PG_NUM_PORTS; p = p + 1) begin: rst_p
-         always @(posedge clk) port_rst_n_q1[link][p] <= port_rst_n[link];
+         always @(posedge clk) port_rst_n_q1[link][p] <= port_rst_n[link][p];
          always @(posedge clk) port_rst_n_q2[link][p] <= port_rst_n_q1[link][p] && rst_n_q1;
 
          // Multi-cycle duplication tree
