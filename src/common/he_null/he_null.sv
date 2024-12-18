c0cabfc0b268e1e83a5028b26f05aa3a1b7ba93c
Michael Adler <Michael.Adler@intel.com>
2024-12-18 18:18:16 -0500

    Use 256 bit PCIe bus when appropriate (#1581)
    - Reduce minimum PCIe bus size to 256 bits. FIM fabric, the PIM and now HE NULL
      all allow it.
    - Update HE NULL:
      - Replace sketchy hand-written pipeliness with standard FIM modules.
      - Handle 256 bit bus correctly.

--- src/common/he_null/he_null.sv
+++ src/common/he_null/he_null.sv
@@ -34,7 +34,6 @@ module he_null #(
    // ----------- Parameters -------------
    localparam END_OF_LIST           = 1'h0;  // Set this to 0 if there is another DFH beyond this
    localparam NEXT_DFH_BYTE_OFFSET  = 24'h0; // Next DFH Byte offset
-   localparam CSR_DEPTH_LOG2        = $clog2(CSR_DEPTH);
    localparam PCIE_TDATA_WIDTH      = ofs_fim_cfg_pkg::PCIE_TDATA_WIDTH;
    localparam PCIE_TUSER_WIDTH      = ofs_fim_cfg_pkg::PCIE_TUSER_WIDTH;
 
@@ -61,127 +60,98 @@ module he_null #(
    // ---- Logic / Struct Declarations ---
    logic mmio_wr_en;
    logic mmio_rd_en;
-   logic axi_rx_if_sop;
-   logic axi_rx_if_cpld;
-   logic rx_sop_init;
-   logic rx_sop_valid;
-   logic axi_rx_if_tvalid_t1;
-   logic axi_rx_if_tready_t1;
-   logic [CSR_ADDR_WIDTH-5:0]     rx_addr, mmio_csr_addr_4b;
+   logic rx_sop;
+   logic [CSR_ADDR_WIDTH-5:0]     rx_addr;
    logic [CSR_ADDR_WIDTH-1:0]     rx_addr_decode;
-   logic [CSR_ADDR_WIDTH-2:0]     mmio_csr_addr_8b;
-   logic [255:0]                  mmio_wr_data, axi_rx_if_data_t1;
+   logic [CSR_DATA_WIDTH-1:0]     mmio_wr_data;
    logic                          mmio_hdr_len;
    PCIe_PUCplHdr_t                cpl_hdr;
-   PCIe_PUReqHdr_t                mmio_hdr_t1;
-   PCIe_PUReqHdr_t                rx_hdr;
+   PCIe_PUReqHdr_t                mmio_hdr;
    logic [CSR_DATA_WIDTH-1:0]     scratch_reg;
    logic [CSR_DATA_WIDTH-1:0]     debug_default_reg;
-   logic flr_rst_n_d;
    logic access_upper32b;
 
-   pcie_ss_axis_if #(
-      .DATA_W (PCIE_TDATA_WIDTH),
-      .USER_W (PCIE_TUSER_WIDTH)
-   ) axi_rx_if (.clk(clk), .rst_n(rst_n));
+   // Register input and output streams. Use simple buffers with a bubble since throughput
+   // is unimportant for the NULL AFU.
+   pcie_ss_axis_if #(.DATA_W (PCIE_TDATA_WIDTH), .USER_W (PCIE_TUSER_WIDTH)) axi_rx_reg (.clk(clk), .rst_n(rst_n));
+   pcie_ss_axis_if #(.DATA_W (PCIE_TDATA_WIDTH), .USER_W (PCIE_TUSER_WIDTH)) axi_tx_reg (.clk(clk), .rst_n(rst_n));
 
-   pcie_ss_axis_if #(
-      .DATA_W (PCIE_TDATA_WIDTH),
-      .USER_W (PCIE_TUSER_WIDTH)
-   ) axi_tx_if (.clk(clk), .rst_n(rst_n));
+   ofs_fim_axis_pipeline#(.MODE(2)) rx_reg(.clk, .rst_n, .axis_s(i_rx_if), .axis_m(axi_rx_reg));
+   ofs_fim_axis_pipeline#(.MODE(2)) tx_reg(.clk, .rst_n, .axis_s(axi_tx_reg), .axis_m(o_tx_if));
+
+   // Ensure that TLP headers and MMIO data are delivered together in a single cycle by checking
+   // that the AXI-S data bus is wide enough for both. If the bus is already wide enough, the code
+   // reduces to just wires.
+   localparam TDATA_WIDTH = (PCIE_TDATA_WIDTH >= pcie_ss_hdr_pkg::HDR_WIDTH*2) ? PCIE_TDATA_WIDTH : pcie_ss_hdr_pkg::HDR_WIDTH*2;
+   pcie_ss_axis_if #(.DATA_W (TDATA_WIDTH), .USER_W (PCIE_TUSER_WIDTH)) axi_rx_if (.clk(clk), .rst_n(rst_n));
+   pcie_ss_axis_if #(.DATA_W (TDATA_WIDTH), .USER_W (PCIE_TUSER_WIDTH)) axi_tx_if (.clk(clk), .rst_n(rst_n));
+
+   ofs_fim_pcie_bus_width rx_width (.i_if(axi_rx_reg), .o_if(axi_rx_if));
+   ofs_fim_pcie_bus_width tx_width (.i_if(axi_tx_if), .o_if(axi_tx_reg));
+
+
+   // The remainder of the logic operates on axi_rx_if/axi_tx_if, which are guaranteed to hold
+   // header+MMIO data in a single cycle and are isolated by registers from the FIM for timing.
+
+   assign mmio_hdr = PCIe_PUReqHdr_t'(axi_rx_if.tdata);
 
    always_comb begin
-      // AXI RX Interface
-      i_rx_if.tready           = i_rx_if.rst_n ? 1'b1 : 0;      
-      axi_rx_if.tvalid         = i_rx_if.tvalid;
-      axi_rx_if.tlast          = i_rx_if.tlast;
-      axi_rx_if.tuser_vendor   = i_rx_if.tuser_vendor;
-      axi_rx_if.tdata          = i_rx_if.tdata;
-      axi_rx_if.tkeep          = i_rx_if.tkeep;
-      axi_rx_if.tready         = i_rx_if.tready;
-
-      // AXI TX Interface
-      axi_tx_if.tready     = o_tx_if.tready;
-      o_tx_if.tvalid       = axi_tx_if.tvalid;
-      o_tx_if.tlast        = axi_tx_if.tlast;
-      o_tx_if.tuser_vendor = axi_tx_if.tuser_vendor;
-      o_tx_if.tdata        = axi_tx_if.tdata;
-      o_tx_if.tkeep        = axi_tx_if.tkeep;
-   end
-
-   always_comb begin 
-      axi_rx_if_sop    = rx_sop_init | rx_sop_valid;
-      mmio_hdr_len     = (mmio_hdr_t1.length > 1) ? 1'b1 : 1'b0; //Length is in DW
-      rx_addr          = (mmio_hdr_t1.fmt_type == M_RD | mmio_hdr_t1.fmt_type == M_WR) ? mmio_hdr_t1.host_addr_h[13:2] : mmio_hdr_t1.host_addr_l[11:0]; //8B aligned
-      rx_addr_decode   = 0;
-      rx_addr_decode   = {2'b0, rx_addr[CSR_ADDR_WIDTH-7:1], 3'b0};
-      mmio_csr_addr_4b = rx_addr;
-      mmio_csr_addr_8b = {'0, mmio_csr_addr_4b[CSR_ADDR_WIDTH-5:1]};
+      mmio_hdr_len     = (mmio_hdr.length > 1) ? 1'b1 : 1'b0; //Length is in DW
+      rx_addr          = func_is_addr32(mmio_hdr.fmt_type) ? mmio_hdr.host_addr_h[13:2] : mmio_hdr.host_addr_l[11:0]; //4B aligned
+      rx_addr_decode   = {'0, rx_addr[CSR_ADDR_WIDTH-7:1], 3'b0};
 
       // CSR Write
-      mmio_wr_en   = axi_rx_if_sop & axi_rx_if_tvalid_t1 & axi_rx_if_tready_t1 & (mmio_hdr_t1.fmt_type == M_WR | mmio_hdr_t1.fmt_type == DM_WR);
-      mmio_wr_data = axi_rx_if_data_t1;
+      mmio_wr_en       = rx_sop & axi_rx_if.tvalid & axi_rx_if.tready & func_is_mwr_req(mmio_hdr.fmt_type);
+      mmio_wr_data     = axi_rx_if.tdata[HDR_WIDTH +: CSR_DATA_WIDTH];
 
       // CSR Read
-      mmio_rd_en    = axi_rx_if_sop & axi_rx_if_tvalid_t1 & axi_rx_if_tready_t1 & (mmio_hdr_t1.fmt_type == M_RD | mmio_hdr_t1.fmt_type == DM_RD);
-      access_upper32b = ~mmio_hdr_len & mmio_csr_addr_4b[0];
+      mmio_rd_en       = rx_sop & axi_rx_if.tvalid & axi_rx_if.tready & func_is_mrd_req(mmio_hdr.fmt_type);
+      access_upper32b  = ~mmio_hdr_len & rx_addr[0];
 
       // Set up completion packet
       cpl_hdr               = 'h0;
       cpl_hdr.fmt_type      = DM_CPL;
 
-      cpl_hdr.attr[8]       = mmio_hdr_t1.attr[8];  // Attribite bits 
-      cpl_hdr.attr[3:2]     = mmio_hdr_t1.attr[3:2];// Attribite bits 
+      cpl_hdr.attr[8]       = mmio_hdr.attr[8];  // Attribite bits
+      cpl_hdr.attr[3:2]     = mmio_hdr.attr[3:2];// Attribite bits
 
-      cpl_hdr.TC            = mmio_hdr_t1.TC;       // Traffic Class
-      cpl_hdr.cpl_status    = 3'h0;                 // Successful Completion
-      cpl_hdr.pf_num        = mmio_hdr_t1.pf_num;
-      cpl_hdr.vf_num        = mmio_hdr_t1.vf_num;
-      cpl_hdr.vf_active     = mmio_hdr_t1.vf_active;
+      cpl_hdr.TC            = mmio_hdr.TC;       // Traffic Class
+      cpl_hdr.cpl_status    = 3'h0;              // Successful Completion
+      cpl_hdr.pf_num        = mmio_hdr.pf_num;
+      cpl_hdr.vf_num        = mmio_hdr.vf_num;
+      cpl_hdr.vf_active     = mmio_hdr.vf_active;
 
-      cpl_hdr.length        = (mmio_hdr_t1.length == 1) ? 10'd1 : 10'd2; //DWs
-      
-      cpl_hdr.tag_h         = mmio_hdr_t1.tag_h;
-      cpl_hdr.tag_m         = mmio_hdr_t1.tag_m;
-      cpl_hdr.tag_l         = mmio_hdr_t1.tag_l;
+      cpl_hdr.length        = (mmio_hdr.length == 1) ? 10'd1 : 10'd2; //DWs
 
-      cpl_hdr.byte_count    = (mmio_hdr_t1.length == 1) ? 10'd4 : 10'd8; //Bytes 
+      cpl_hdr.tag_h         = mmio_hdr.tag_h;
+      cpl_hdr.tag_m         = mmio_hdr.tag_m;
+      cpl_hdr.tag_l         = mmio_hdr.tag_l;
+
+      cpl_hdr.byte_count    = (mmio_hdr.length == 1) ? 10'd4 : 10'd8; //Bytes 
       cpl_hdr.comp_id       = {'0,
-                               mmio_hdr_t1.vf_num,
-                               mmio_hdr_t1.vf_active,
-                               mmio_hdr_t1.pf_num};
+                               mmio_hdr.vf_num,
+                               mmio_hdr.vf_active,
+                               mmio_hdr.pf_num};
 
-      cpl_hdr.low_addr      = ((mmio_hdr_t1.fmt_type == M_RD) | (mmio_hdr_t1.fmt_type == M_WR)) ? 
-                                 {mmio_hdr_t1.host_addr_h[6:2], 2'b00} : 
-                                 {mmio_hdr_t1.host_addr_l[4:0], 2'b00};
-      cpl_hdr.req_id        = mmio_hdr_t1.req_id; //Response with same req_id as request
+      cpl_hdr.low_addr      = func_is_addr32(mmio_hdr.fmt_type) ?
+                                 {mmio_hdr.host_addr_h[6:2], 2'b00} :
+                                 {mmio_hdr.host_addr_l[4:0], 2'b00};
+      cpl_hdr.req_id        = mmio_hdr.req_id; //Response with same req_id as request
    end
 
-   //Create SOP & separate data/header signals
    always_ff @(posedge clk) begin
-      if(~rst_n) begin
-         rx_sop_init         <= 1'b1;
-         rx_sop_valid        <= 1'b0;
-         axi_rx_if_tvalid_t1 <= '0;
-         axi_rx_if_tready_t1 <= '0;
-         mmio_hdr_t1         <= '0;
-         axi_rx_if_data_t1   <= '0;
-      end else begin
-         if(rx_sop_init & axi_rx_if.tvalid) begin
-            rx_sop_init <= 1'b0;
-         end
-         if(axi_rx_if.tvalid & axi_rx_if.tready) begin
-            rx_sop_valid <= axi_rx_if.tlast;
-         end
+      if (axi_rx_if.tvalid & axi_rx_if.tready)
+         rx_sop <= axi_rx_if.tlast;
 
-         axi_rx_if_tvalid_t1 <= axi_rx_if.tvalid;
-         axi_rx_if_tready_t1 <= axi_rx_if.tready;
-         mmio_hdr_t1         <= axi_rx_if.tdata[255:0];
-         axi_rx_if_data_t1   <= axi_rx_if.tdata[256+:CSR_DATA_WIDTH];
-      end
+      if (~rst_n)
+         rx_sop <= 1'b1;
    end
 
-   // Receive MMIO Writes 
+   // Because of ofs_fim_axis_pipeline in the TX path we can be sure that the axi_tx_if_.tready value
+   // is independent of axi_tx_if.tvalid. There is no need to check whether axi_rx_if is a read request.
+   assign axi_rx_if.tready = axi_tx_if.tready;
+
+   // Receive MMIO Writes
    always_ff @(posedge clk) begin
       if (~rst_n) begin
          scratch_reg       <= '0;
@@ -191,9 +161,12 @@ module he_null #(
             case ( {rx_addr_decode} )
                NULL_SCRATCHPAD: begin
                   if (access_upper32b)
-                     scratch_reg <= {axi_rx_if_data_t1[31:0], axi_rx_if_data_t1[31:0]};
-                  else
-                     scratch_reg <= axi_rx_if_data_t1;
+                     scratch_reg[63:32] <= mmio_wr_data[31:0];
+                  else begin
+                     scratch_reg[31:0] <= mmio_wr_data[31:0];
+                     if (mmio_hdr_len)
+                        scratch_reg[63:32] <= mmio_wr_data[63:32];
+                  end
                end
                default: begin
                   scratch_reg       <= 'haaaa_aaaa;
@@ -204,57 +177,46 @@ module he_null #(
       end
    end
 
-   // MMIO Completion 
-   always_ff @(posedge clk) begin
-      if (~rst_n) begin
-         axi_tx_if.tvalid         <= 0;
-         axi_tx_if.tlast          <= 0;
-         axi_tx_if.tuser_vendor   <= 'h1;
-         axi_tx_if.tdata          <= 0;
-         axi_tx_if.tkeep          <= 0;
-      end else begin
-         if (axi_tx_if.tvalid) begin
-            axi_tx_if.tvalid         <= (axi_tx_if.tvalid  && axi_tx_if.tready) ? '0  : 1; 
-            axi_tx_if.tlast          <= (axi_tx_if.tvalid  && axi_tx_if.tready) ? '0  : axi_tx_if.tlast;
-            axi_tx_if.tuser_vendor   <= (axi_tx_if.tvalid  && axi_tx_if.tready) ? 'h1 : 'h0;
-            axi_tx_if.tkeep          <= (axi_tx_if.tvalid  && axi_tx_if.tready) ? '1  : axi_tx_if.tkeep;
-            axi_tx_if.tdata          <= (axi_tx_if.tvalid  && axi_tx_if.tready) ? '0  : axi_tx_if.tdata;
-         end else begin
-            axi_tx_if.tvalid         <= axi_tx_if.tvalid;
-            axi_tx_if.tlast          <= axi_tx_if.tlast;
-            axi_tx_if.tuser_vendor   <= axi_tx_if.tuser_vendor;
-            axi_tx_if.tkeep          <= axi_tx_if.tkeep;
-            axi_tx_if.tdata          <= axi_tx_if.tdata;
-         end
-         if (mmio_rd_en) begin
-            axi_tx_if.tdata[255:0] <= cpl_hdr;
-            if (~axi_tx_if.tvalid | axi_tx_if.tready) begin
-               axi_tx_if.tvalid <= 1;
-               axi_tx_if.tlast  <= 1;
-               axi_tx_if.tuser_vendor   <= 'h0;
-               axi_tx_if.tkeep  <= (cpl_hdr.length == 'd1) ? {'0, {(4){1'b1}}, {(PCIE_TDATA_WIDTH/16){1'b1}} } : 
-                                                      {'0, {(8){1'b1}}, {(PCIE_TDATA_WIDTH/16){1'b1}} } ; 
-               axi_tx_if.tdata[511:256] <= '0;
-               case ( {rx_addr_decode} )
-                  NULL_DFH: begin
-                     axi_tx_if.tdata[511:256] <= access_upper32b ? DEF_VIO_FEATURE_DFH[63:32] : DEF_VIO_FEATURE_DFH; 
-                  end
-                  NULL_GUID_L_ADDR: begin
-                     axi_tx_if.tdata[511:256] <= access_upper32b ?  HE_GUID_L[63:32] : HE_GUID_L; 
-                  end
-                  NULL_GUID_H_ADDR: begin
-                     axi_tx_if.tdata[511:256] <= access_upper32b ? HE_GUID_H[63:32] :  HE_GUID_H; 
-                  end
-                  NULL_SCRATCHPAD: begin
-                     axi_tx_if.tdata[511:256] <= access_upper32b ? scratch_reg[63:32] :  scratch_reg; 
-                  end
-                  default: begin
-                     axi_tx_if.tdata[511:256] <= {debug_default_reg, 1'b0, rx_addr_decode};
-                  end
-               endcase
-            end
-         end
+   // MMIO Completion
+   logic [63:0] cpl_data;
+
+   always_comb begin
+      case ( {rx_addr_decode} )
+      NULL_DFH: begin
+         cpl_data = DEF_VIO_FEATURE_DFH;
       end
+      NULL_GUID_L_ADDR: begin
+         cpl_data = HE_GUID_L;
+      end
+      NULL_GUID_H_ADDR: begin
+         cpl_data = HE_GUID_H;
+      end
+      NULL_SCRATCHPAD: begin
+         cpl_data = scratch_reg;
+      end
+      default: begin
+         cpl_data = '0;
+         cpl_data[2 +: CSR_ADDR_WIDTH] = rx_addr;
+         cpl_data[34 +: CSR_ADDR_WIDTH] = rx_addr;
+      end
+      endcase
+
+      if (access_upper32b) begin
+         cpl_data[31:0] = cpl_data[63:32];
+      end
+   end
+
+   always_comb begin
+      axi_tx_if.tvalid = mmio_rd_en;
+      axi_tx_if.tdata = { cpl_data, cpl_hdr };
+      axi_tx_if.tlast = 1'b1;
+      axi_tx_if.tuser_vendor = '0;
+
+      axi_tx_if.tkeep = { '0,
+                          mmio_hdr_len ? 4'hf : 4'h0,  // High 32 bits cpl_data
+                          4'hf,                        // Low 32 bits cpl_data
+                          {(HDR_WIDTH/8){1'b1}}        // cpl_hdr
+                        };
    end
 
 endmodule
